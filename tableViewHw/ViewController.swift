81ec9ee90320841840892d728fdea68e5b0a1951
Flavius Bortas <flavius.bortas@detroitlabs.com>
2018-08-28 08:31:51 -0400

    this is the commit

--- tableViewHw/ViewController.swift
+++ tableViewHw/ViewController.swift
@@ -8,18 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ChecklistViewController: UITableViewController {
+    
+    var checklistItems = ["Watch The Office", "Eat some alfredo", "Learn Git :(", "Go to the Gym", "Go to bed"]
+    var numberOfRows = 5
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return checklistItems.count
     }
-
-
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        
+        cell.textLabel?.text = checklistItems[indexPath.row]
+        
+        return cell
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            checklistItems.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .left)
+        }
+    }
+    
 }
+
 
