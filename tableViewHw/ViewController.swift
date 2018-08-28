2be47fedb1a72284ae005e8c347183938eeaf8e4
Flavius Bortas <flavius.bortas@detroitlabs.com>
2018-08-27 21:43:12 -0400

    First commit

---
+++ tableViewHw/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tableViewHw
+//
+//  Created by Flavius Bortas on 8/27/18.
+//  Copyright © 2018 Flavius Bortas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
